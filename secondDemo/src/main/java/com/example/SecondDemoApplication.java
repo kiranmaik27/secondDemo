317e852374f9b45180478a09eca6716655e4bff2
Kiran <kiranmai.k27@gmail.com>
2016-08-04 08:36:27 -0400

    Initial commit

---
+++ secondDemo/src/main/java/com/example/SecondDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondDemoApplication.class, args);
+	}
+}
